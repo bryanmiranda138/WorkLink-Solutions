9c0af1e16fdb378bce15071875a59418ad21b01e
Montes-Tobar <mt17005@ues.edu.sv>
2025-05-04 12:52:21 -0600

    Agregado tipo de usuario en formulario de registro

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <a href="{{ route('postulantes.index') }}" class="text-blue-500 hover:underline">
-                Ver lista de postulantes
-            </a>                
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">              
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
                 </div>
